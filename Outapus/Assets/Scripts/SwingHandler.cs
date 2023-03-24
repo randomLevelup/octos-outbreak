cc4621fe1ed9d3aac0ae07fcfc802f06668cb03e
Jake Kerrigan <36115570+Jakedude18@users.noreply.github.com>
2023-03-23 22:32:45 -0400

    Added dynamic swinging

--- Outapus/Assets/Scripts/SwingHandler.cs
+++ Outapus/Assets/Scripts/SwingHandler.cs
@@ -7,6 +7,7 @@ public class SwingHandler : MonoBehaviour
     public GameObject octopus;
     private SpringJoint2D joint;
     public Rigidbody2D connectionBody;
+    public Transform crosshair;
     bool swinging = true;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,15 @@ public class SwingHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //update cross hair
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 crosshairPos = new Vector3(mousePos.x, mousePos.y,0);
+        
+        crosshair.position = crosshairPos;
+        if(Input.GetMouseButtonDown(0)){
+            connectionBody.position = crosshairPos;
+        }
+
         if(Input.GetKeyUp(KeyCode.Space))
         {
             if(swinging){
