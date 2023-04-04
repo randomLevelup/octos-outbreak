0c678c4736c7ed68b513924b25f001d7e212d867
Jupiter <jwestbard@gmail.com>
2023-04-04 15:36:16 -0400

    octo head physics

--- Outapus/Assets/Jupiter/Scripts/HeadController.cs
+++ Outapus/Assets/Jupiter/Scripts/HeadController.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HeadController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform ikBase;
+    public Transform ikTarget;
+    public Transform bobbleHeadObject;
+    public float heightOffset = 2f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position = ikBase.position + new Vector3(0, heightOffset);
+        ikTarget.position = bobbleHeadObject.position;
     }
 }
