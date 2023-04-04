097b4ecd25d7c409da2b0b540049ec1e2aac0546
Jupiter <jwestbard@gmail.com>
2023-04-04 18:25:45 -0400

    build numba 2

--- Outapus/Assets/Scripts/Jake_scripts/SwingHandler.cs
+++ Outapus/Assets/Scripts/Jake_scripts/SwingHandler.cs
@@ -15,7 +15,7 @@ public class SwingHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        joint = octopus.GetComponent<SpringJoint2D>();
+        joint = octopus.GetComponents<SpringJoint2D>()[1];
     }
 
     // Update is called once per frame
