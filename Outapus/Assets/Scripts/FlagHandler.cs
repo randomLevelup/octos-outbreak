7a65b6d7cbcc948fea032ac585f28e3976b1d64f
Jupiter <jwestbard@gmail.com>
2023-04-10 16:00:59 -0400

    basic scene template again

---
+++ Outapus/Assets/Scripts/FlagHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlagHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int numScenes = 5;
+    public static int curScene = 0;
+
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            nextGame();
+        }
+    }
+    public void nextGame()
+    {
+        curScene++;
+        curScene = curScene % numScenes;
+        
+        SceneManager.LoadScene(curScene);
+    }
+}
