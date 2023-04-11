0d4ce28a5574b8fb7dcc7e2888094f4cef4f0d93
Jake Kerrigan <36115570+Jakedude18@users.noreply.github.com>
2023-04-11 16:09:23 -0400

    updated swinger and made staticSwingerScript which allows octapus to interact with multiple hinges

---
+++ Outapus/Assets/Scripts/Jake_scripts/StaticSwingHandler.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticSwingHandler : MonoBehaviour
+{
+    public GameObject hingeFolder;
+    public Transform abdomenObject;
+    public LineRenderer lr;
+    public float baseBubbleSize = 3.74f;
+    public bool swinging = false;
+    private SpringJoint2D[] allJoints;
+    public GameObject SwingController;
+    private LineController lineControllerScript;
+
+
+    private SpringJoint2D currentJoint;
+    private Rigidbody2D abdomenRB;
+
+    private void Start()
+    {
+        allJoints = hingeFolder.GetComponentsInChildren<SpringJoint2D>();
+        abdomenRB = abdomenObject.GetComponent<Rigidbody2D>();
+        lr.enabled = false;
+        lineControllerScript = SwingController.GetComponent<LineController>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!swinging)
+            {
+                int closestIndex = nearestJointIndex();
+                currentJoint = allJoints[closestIndex];
+                float swingRadius = CalculateRadius(currentJoint.gameObject);
+
+                if (Vector3.Distance(currentJoint.transform.position, abdomenObject.position) < swingRadius)
+                {
+                    lineControllerScript.hinge = currentJoint.transform;
+                    currentJoint.connectedBody = abdomenRB;
+                    swinging = true;
+                    lr.enabled = true;
+                }
+
+            } 
+            else
+            {
+                currentJoint.connectedBody = null;
+                swinging = false;
+                lr.enabled = false;
+                
+            }
+        }
+    }
+
+    private float CalculateRadius(GameObject hingeObject)
+    {
+        Transform bubbleTransform = hingeObject.GetComponentsInChildren<Transform>()[1];
+        return bubbleTransform.localScale.x * baseBubbleSize;
+    }
+    
+    private int nearestJointIndex(){
+        int nearestIndex = 0;
+        float smallestDistance = float.MaxValue;
+        for(int i = 0; i < allJoints.Length; i++){
+            float distance = Vector3.Distance (allJoints[i].transform.position, abdomenObject.position);
+            if(distance < smallestDistance){
+                smallestDistance = distance;
+                nearestIndex = i;
+            } 
+        }
+        return nearestIndex;
+    }
+}
