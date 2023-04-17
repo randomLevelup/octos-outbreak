7f1dd83a3d734b08dafdde45796350150df2b3c4
Jupiter <jwestbard@gmail.com>
2023-04-16 23:04:37 -0400

    fixed the corner issue

--- Outapus/Assets/Scripts/rbIsGrounded.cs
+++ Outapus/Assets/Scripts/rbIsGrounded.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class rbIsGrounded : MonoBehaviour
 {
+    public int numWallsTouching = 0;
     public bool isGrounded = false;
     public bool jumpPower = false;
 
     private void OnCollisionEnter2D(Collision2D c)
     {
-        if (c.gameObject.CompareTag("floor")) { isGrounded = true; }
+        if (c.gameObject.CompareTag("floor")) { numWallsTouching++; isGrounded = true; }
         if (c.gameObject.CompareTag("jumpPower")) { jumpPower = true; }
     }
 
     private void OnCollisionExit2D(Collision2D c)
     {
-        if (c.gameObject.CompareTag("floor")) { isGrounded = false; }
+        if (c.gameObject.CompareTag("floor")) { 
+            numWallsTouching--;
+            isGrounded = numWallsTouching > 0;
+        }
     }
 }
