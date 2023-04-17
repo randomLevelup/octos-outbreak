5ad730059ebeeddc2f354d26a780aec75c169c30
Jake Kerrigan <36115570+Jakedude18@users.noreply.github.com>
2023-04-17 18:19:56 -0400

    ground NPCs

---
+++ Outapus/Assets/Scripts/Jake_scripts/GroundNPCMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundNPCMovement : MonoBehaviour
+{
+  // https://www.youtube.com/watch?v=2SXa10ILJms
+    public GameObject player;
+    public float speed;
+    public float jumpHeight;
+    private float distance;
+    private Rigidbody2D rb;
+    private rbIsGrounded controlState;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        controlState = this.GetComponent<rbIsGrounded>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+        if(angle > 0){
+            Jump();
+        }
+        rb.AddForce(new Vector3(direction.x, 0, 0));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    
+    void Jump(){
+        if(controlState.isGrounded){
+            rb.AddForce(new Vector3(0, jumpHeight, 0));
+        }
+    }
+}
