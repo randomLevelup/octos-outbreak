588d3baf23a7889ac1729557672f749db2e31674
heeheemcgee <67600753+michaelhu22@users.noreply.github.com>
2023-05-09 16:50:18 -0400

    add kill sound

--- Outapus/Assets/Scripts/Jake_scripts/Kills.cs
+++ Outapus/Assets/Scripts/Jake_scripts/Kills.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class Kills : MonoBehaviour
 {
+    public AudioSource deathSound;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log(collision.gameObject);
         if (collision.gameObject.CompareTag("Player"))
         {
+            deathSound.Play();
             JupiterGameHandler gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<JupiterGameHandler>();
             gameHandler.DestroyClones();
             gameHandler.InitializeLevel();
