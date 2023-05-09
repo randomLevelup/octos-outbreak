c558b9d47b2b8d36701cb1bd1c5d90efc308a59f
Matthew Johnson <johnsonmh36@gmail.com>
2023-05-09 16:40:20 -0400

    Boolean fix

--- Outapus/Assets/Jupiter/Scripts/LevelInterface.cs
+++ Outapus/Assets/Jupiter/Scripts/LevelInterface.cs
@@ -10,5 +10,5 @@ public class LevelInterface : MonoBehaviour
     public GameObject BGParallax;
     public Transform cameraBounds;
     public Transform lightSource;
-    public boolean metalGearSound;
+    public bool metalGearSound;
 }
