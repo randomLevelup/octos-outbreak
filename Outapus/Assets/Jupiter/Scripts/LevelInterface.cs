62d826183fe7182e532c6a7159344558ad92ee5e
Jake Kerrigan <36115570+Jakedude18@users.noreply.github.com>
2023-05-09 16:49:24 -0400

    stable build wooooo

--- Outapus/Assets/Jupiter/Scripts/LevelInterface.cs
+++ Outapus/Assets/Jupiter/Scripts/LevelInterface.cs
@@ -10,5 +10,5 @@ public class LevelInterface : MonoBehaviour
     public GameObject BGParallax;
     public Transform cameraBounds;
     public Transform lightSource;
-    public bool metalGearSound;
+    public bool solidSnake;
 }
